399cfaff6db08eec74ff0d32570f9905d2490d41
ajaberi <adnan.ajaberi@gmail.com>
2023-09-16 17:29:24 +0300

    fixed new line issue

--- pkg/broadcast.go
+++ pkg/broadcast.go
@@ -7,7 +7,7 @@ func BroadcastMessages() {
 		msg := <-messages
 		for _, client := range Clients {
 			if msg.Socket != client.Socket {
-				client.Socket.Write([]byte("\n" + "[" + time.Now().Format("2006-01-02 15:04:05") + "][" + msg.Name + "]: " + msg.Message))
+				client.Socket.Write([]byte("\n" + "[" + time.Now().Format("2006-01-02 15:04:05") + "][" + msg.Name + "]: " + msg.Message + "\n"))
 				client.Socket.Write([]byte("[" + time.Now().Format("2006-01-02 15:04:05") + "][" + client.Name + "]: "))
 			} else {
 				client.Socket.Write([]byte("[" + time.Now().Format("2006-01-02 15:04:05") + "][" + client.Name + "]: "))
